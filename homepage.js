5ffa41389753010f874c4a8352d4d13a73126fb4
anmester09 <58737061+anmester09@users.noreply.github.com>
2019-12-11 13:45:20 -0500

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,24 @@
+// window.addEventListener('load', (event) => {
+//     addNavListeners()
+// });
+
+// //add event listeners to the nav-bar
+// function addNavListeners() {
+//     const navBar = document.getElementById('navbar')
+//     console.log('adding event listeners', navBar)
+//     navBar.addEventListener('click', chooseNav)
+// }
+
+// //this function is called with navbar is clicked
+// function chooseNav(event) {
+//     const navSelect = event.target.id
+//     if (navSelect === 'men') {
+//         console.log(navSelect)
+//     } else if (navSelect === 'women') {
+//         console.log(navSelect)
+//     } else if (navSelect === 'kids') {
+//         console.log(navSelect)
+//     } else if (navSelect === 'home') {
+//         console.log(navSelect)
+//     }
+// }
