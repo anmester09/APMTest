2e6831ffaf570c6bc53e19f2eae21110307c23a3
anmester09 <58737061+anmester09@users.noreply.github.com>
2019-12-13 12:11:05 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -7,7 +7,6 @@ window.addEventListener('load', (event) => {
 function addButtonListeners() {
     let purchaseButtons = document.querySelectorAll("#purchase");
     let addToCartButtons = document.querySelectorAll('#add-to-cart')
-    console.log('purchase buttons', purchaseButtons, 'add to cart buttons', addToCartButtons)
     
     purchaseButtons.forEach(function(elem) {
         elem.addEventListener("click", recordPurchase);
