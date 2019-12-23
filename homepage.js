eeee0471c9c43e87d7fa21770c59d271d1ccfa66
anmester09 <58737061+anmester09@users.noreply.github.com>
2019-12-23 13:30:05 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,21 +1,33 @@
+var quizStart = document.querySelector('#start-quiz')
+let userData =  {}
+
+// initiate event listeners on page load
 window.addEventListener('load', (event) => {
     addButtonListeners()
-    console.log('hi')
 });
 
 //add event listeners to purchase button
 function addButtonListeners() {
     let purchaseButtons = document.querySelectorAll("#purchase");
     let addToCartButtons = document.querySelectorAll('#add-to-cart')
+    let menShirtProductPage = document.getElementById('#see-more')
+    let quizStart = document.querySelector('#start-quiz')
+
+    console.log('purchase buttons', purchaseButtons)
+    console.log('add to cart', addToCartButtons)
+
+    quizStart.addEventListener('click', startQuiz)
+
+    menShirtProductPage.addEventListener('click', goToProductPage)
     
     purchaseButtons.forEach(function(elem) {
         elem.addEventListener("click", recordPurchase);
-        console.log('purchase event added')
+        console.log('inside purchase')
     });
 
     addToCartButtons.forEach(function(elem) {
         elem.addEventListener("click", recordAddToCart);
-        console.log('add to cart event added')
+        console.log('inside cart')
     });
 }
 
@@ -23,7 +35,7 @@ function addButtonListeners() {
 function recordPurchase(event) {
     let targetProductSKU = event.target.dataset.sku
     let targetItemPrice = event.target.dataset.price
-    console.log('recording purchase')
+
     DY.API("event", {
         name: "Purchase",
         properties: {
@@ -41,11 +53,10 @@ function recordPurchase(event) {
     })
 }
 
-//fire event to record add to cart
+// fire event to record add to cart
 function recordAddToCart(event) {
     let targetProductSKU = event.target.dataset.sku
     let targetItemPrice = event.target.dataset.price
-    console.log('adding to cart')
 
     DY.API("event", {
         name: "Add to Cart",
@@ -62,4 +73,79 @@ function recordAddToCart(event) {
             ]
         }
     })
+}
+
+// go to mens shirt product page
+function goToProductPage(event) {
+    console.log('hi')
+}
+
+// start quiz
+function startQuiz(event) {
+    let targetEl = event.target.parentNode
+    targetEl.style.display = 'none'
+
+    let quizQuestionOne = document.querySelector('.card-body.quiz-q1')
+    quizQuestionOne.style.visibility = ''
+
+    let quizQ1Answers = document.querySelectorAll('#question-1')
+    quizQ1Answers.forEach(function(elem) {
+        elem.addEventListener("click", quizQuestion1);
+    });
+    
+}
+
+function quizQuestion1(event) {
+    console.log(event.target.dataset.id)
+
+    userData.q1 = event.target.dataset.id
+
+    let targetEl = event.target.parentNode
+    targetEl.style.display = 'none'
+
+    let quizQuestionTwo = document.querySelector('.card-body.quiz-q2')
+    quizQuestionTwo.style.visibility = ''
+
+    let quizQ2Answers = document.querySelectorAll('#question-2')
+    quizQ2Answers.forEach(function(elem) {
+        elem.addEventListener("click", quizQuestion2);
+    });
+}
+
+function quizQuestion2(event) {
+    console.log(event.target.dataset.id)
+
+    userData.q2 = event.target.dataset.id
+
+    let targetEl = event.target.parentNode
+    targetEl.style.display = 'none'
+
+    let quizQuestionThree = document.querySelector('.card-body.quiz-q3')
+    quizQuestionThree.style.visibility = ''
+
+    let quizQ3Answers = document.querySelectorAll('#question-3')
+    quizQ3Answers.forEach(function(elem) {
+        elem.addEventListener("click", quizQuestion3);
+    });
+}
+
+function quizQuestion3(event) {
+    userData.q3 = event.target.dataset.id
+
+    let targetEl = event.target.parentNode
+    targetEl.style.display = 'none'
+
+    let quizFinalPage = document.querySelector('.card-body.quiz-final')
+    quizFinalPage.style.visibility = ''
+
+    document.querySelector('#get-results').addEventListener('click', getQuizRecommendations)
+}
+
+function getQuizRecommendations() {
+    console.log(userData)
+    let targetEl = event.target.parentNode
+    targetEl.style.display = 'none'
+
+    let quizFinalPage = document.querySelector('.card-body.quiz-recs')
+    quizFinalPage.style.visibility = ''
 }
